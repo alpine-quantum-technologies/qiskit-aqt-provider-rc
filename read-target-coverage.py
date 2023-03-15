20f7a32f2cd731c1c3daa04fe7fd1f69d648a181
Etienne Wodey <44871469+airwoodix@users.noreply.github.com>
2023-03-15 21:33:56 +0100

    Update read-target-coverage.py
    Co-authored-by: Wilfried Huss <84843123+wilfried-huss@users.noreply.github.com>

--- read-target-coverage.py
+++ read-target-coverage.py
@@ -4,6 +4,7 @@ This is used by the Github workflows to write the coverage report comment on PRs
 
 import tomlkit
 
-with open("pyproject.toml") as fp:
-    data = tomlkit.load(fp)
-    print(float(data["tool"]["coverage"]["report"]["fail_under"]) / 100.0)  # type: ignore[index, arg-type]
+if __name__ == "__main__":
+    with open("pyproject.toml") as fp:
+        data = tomlkit.load(fp)
+        print(float(data["tool"]["coverage"]["report"]["fail_under"]) / 100.0)  # type: ignore[index, arg-type]
