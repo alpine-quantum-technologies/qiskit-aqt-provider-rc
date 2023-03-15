0174d3c5cd435dad498e7730066502606d2fa60c
Etienne Wodey <etienne.wodey@aqt.eu>
2023-03-15 16:09:04 +0100

    Support MEASURE and arbitrary-angle RXX operations.

--- qiskit_aqt_provider/transpiler_plugin.py
+++ qiskit_aqt_provider/transpiler_plugin.py
@@ -13,15 +13,11 @@
 import math
 from typing import List, Optional
 
-from qiskit import QuantumCircuit
 from qiskit.circuit.library import RGate
-from qiskit.circuit.quantumregister import Qubit
-from qiskit.converters import circuit_to_dag
 from qiskit.dagcircuit import DAGCircuit
 from qiskit.transpiler.basepasses import BasePass, TransformationPass
 from qiskit.transpiler.passmanager import PassManager
 from qiskit.transpiler.passmanager_config import PassManagerConfig
-from qiskit.transpiler.preset_passmanagers import common
 from qiskit.transpiler.preset_passmanagers.plugin import PassManagerStagePlugin
 
 
@@ -52,57 +48,3 @@ class AQTSchedulingPlugin(PassManagerStagePlugin):
         ]
 
         return PassManager(passes)
-
-
-def arbitrary_rxx_as_xx(theta: float, q0: Qubit, q1: Qubit) -> QuantumCircuit:
-    """Quantum circuit equivalent to Rxx(theta) on q0,q1 in terms of Rxx(π/2)"""
-
-    qr = {q0.register, q1.register}
-    qc = QuantumCircuit(*qr)
-    qc.rx(-math.pi, q0)
-    qc.ry(math.pi / 2, q1)
-    qc.rx(math.pi / 2, q1)
-    qc.rxx(math.pi / 2, q0, q1)
-    qc.rz(theta, q1)
-    qc.rxx(math.pi / 2, q0, q1)
-    qc.ry(-math.pi / 2, q1)
-    qc.rz(math.pi / 2, q1)
-
-    return qc
-
-
-class RewriteRxxAsXx(TransformationPass):
-    """Rewrite synthesized Rxx gates as Rxx(pi/2) gates."""
-
-    def run(self, dag: DAGCircuit) -> DAGCircuit:
-        for node in dag.gate_nodes():
-            if node.name == "rxx":
-                (theta,) = node.op.params
-                if math.isclose(float(theta), math.pi / 2):
-                    continue
-
-                q0, q1 = node.qargs
-                qc = arbitrary_rxx_as_xx(float(theta), q0, q1)
-                dag.substitute_node_with_dag(node, circuit_to_dag(qc))
-
-        return dag
-
-
-class AQTTranslationPlugin(PassManagerStagePlugin):
-    def pass_manager(
-        self, pass_manager_config: PassManagerConfig, optimization_level: Optional[int] = None
-    ) -> PassManager:
-        translation_pm = common.generate_translation_passmanager(
-            target=pass_manager_config.target,
-            basis_gates=pass_manager_config.basis_gates,
-            approximation_degree=pass_manager_config.approximation_degree,
-            coupling_map=pass_manager_config.coupling_map,
-            backend_props=pass_manager_config.backend_properties,
-            unitary_synthesis_method=pass_manager_config.unitary_synthesis_method,
-            unitary_synthesis_plugin_config=pass_manager_config.unitary_synthesis_plugin_config,
-            hls_config=pass_manager_config.hls_config,
-        )
-
-        passes: List[BasePass] = [RewriteRxxAsXx()]
-
-        return translation_pm + PassManager(passes)
--- test/test_transpilation.py
+++ test/test_transpilation.py
@@ -12,15 +12,11 @@
 
 from math import pi
 
-import numpy.testing as npt
 import pytest
 from qiskit import QuantumCircuit, transpile
-from qiskit.circuit.quantumcircuit import QuantumRegister
-from qiskit_aer import AerSimulator
 
 from qiskit_aqt_provider.aqt_resource import AQTResource
 from qiskit_aqt_provider.test.circuits import assert_circuits_equal
-from qiskit_aqt_provider.transpiler_plugin import arbitrary_rxx_as_xx
 
 
 @pytest.mark.parametrize(
@@ -38,9 +34,11 @@ def test_rx_wrap_angle(
     """Check that transpiled rotation gate angles are wrapped to [-π,π]."""
     qc = QuantumCircuit(1)
     qc.rx(angle, 0)
+    qc.measure_all()
 
     expected = QuantumCircuit(1)
     expected.r(expected_angle, 0, 0)
+    expected.measure_all()
 
     result = transpile(qc, offline_simulator_no_noise, optimization_level=3)
     assert_circuits_equal(result, expected)
@@ -50,9 +48,11 @@ def test_rx_r_rewrite_simple(offline_simulator_no_noise: AQTResource) -> None:
     """Check that Rx gates are rewritten as R gates."""
     qc = QuantumCircuit(1)
     qc.rx(pi / 2, 0)
+    qc.measure_all()
 
     expected = QuantumCircuit(1)
     expected.r(pi / 2, 0, 0)
+    expected.measure_all()
 
     result = transpile(qc, offline_simulator_no_noise, optimization_level=3)
     assert_circuits_equal(result, expected)
@@ -63,56 +63,12 @@ def test_decompose_1q_rotations_simple(offline_simulator_no_noise: AQTResource) 
     qc = QuantumCircuit(1)
     qc.rx(pi / 2, 0)
     qc.ry(pi / 2, 0)
+    qc.measure_all()
 
     expected = QuantumCircuit(1)
     expected.rz(-pi / 2, 0)
     expected.r(pi / 2, 0, 0)
+    expected.measure_all()
 
     result = transpile(qc, offline_simulator_no_noise, optimization_level=3)
     assert_circuits_equal(result, expected)
-
-
-def test_decompose_rxx_as_xx_simple(offline_simulator_no_noise: AQTResource) -> None:
-    """Check that arbitrary-angle Rxx gates are rewritten in terms of Rxx(π/2) ones."""
-    qc = QuantumCircuit(2)
-    qc.rxx(pi / 3, 0, 1)
-
-    expected = QuantumCircuit(2)
-    expected.r(-pi, 0, 0)
-    expected.r(pi / 2, pi / 2, 1)
-    expected.r(pi / 2, 0, 1)
-    expected.rxx(pi / 2, 0, 1)
-    expected.rz(pi / 3, 1)
-    expected.rxx(pi / 2, 0, 1)
-    expected.r(-pi / 2, pi / 2, 1)
-    expected.rz(pi / 2, 1)
-
-    # use optimization level 0 such that no other transformation is done
-    result = transpile(qc, offline_simulator_no_noise, optimization_level=0)
-    assert_circuits_equal(result, expected)
-
-
-@pytest.mark.parametrize("theta", [pi / 2, pi / 3, pi / 4, -pi / 3, -pi / 4])
-def test_arbitrary_rxx_decomposition(theta: float) -> None:
-    """Check that `arbitrary_rxx_as_xx(theta)` returns a circuit equivalent
-    to `rxx(theta)` up to a global phase."""
-    backend = AerSimulator(method="unitary")
-
-    qr = QuantumRegister(2)
-    q0, q1 = qr._bits
-    qc = arbitrary_rxx_as_xx(theta, q0, q1)
-    qc.save_state()
-
-    job = backend.run(qc)
-    u_result = job.result().get_unitary(0)
-
-    expected = QuantumCircuit(2)
-    # the decomposition adds a global phase to the unitary
-    expected.global_phase = -pi / 2
-    expected.rxx(theta, 0, 1)
-    expected.save_state()
-
-    job = backend.run(expected)
-    u_expected = job.result().get_unitary(0)
-
-    npt.assert_allclose(u_result.data, u_expected.data, atol=1e-7)
