a14385c0c8c8de9b9b8541cf2ee2c2e75b5b86d7
Etienne Wodey <etienne.wodey@aqt.eu>
2023-03-15 19:17:10 +0100

    transpiler: wrap Rxx angles

--- qiskit_aqt_provider/transpiler_plugin.py
+++ qiskit_aqt_provider/transpiler_plugin.py
@@ -13,11 +13,16 @@
 import math
 from typing import List, Optional
 
+import numpy as np
+from qiskit import QuantumCircuit
 from qiskit.circuit.library import RGate
+from qiskit.circuit.quantumregister import Qubit
+from qiskit.converters import circuit_to_dag
 from qiskit.dagcircuit import DAGCircuit
 from qiskit.transpiler.basepasses import BasePass, TransformationPass
 from qiskit.transpiler.passmanager import PassManager
 from qiskit.transpiler.passmanager_config import PassManagerConfig
+from qiskit.transpiler.preset_passmanagers import common
 from qiskit.transpiler.preset_passmanagers.plugin import PassManagerStagePlugin
 
 
@@ -48,3 +53,67 @@ class AQTSchedulingPlugin(PassManagerStagePlugin):
         ]
 
         return PassManager(passes)
+
+
+def wrap_rxx_angle(theta: float, q0: Qubit, q1: Qubit) -> QuantumCircuit:
+    """Circuit equivalent to RXX(theta, q0, q1) with the RXX angle in [-π/2, π/2]."""
+
+    qr = {q0.register, q1.register}
+
+    if abs(theta) <= math.pi / 2:
+        qc = QuantumCircuit(*qr)
+        qc.rxx(theta, q0, q1)
+        return qc
+
+    theta %= 2 * math.pi
+
+    if abs(theta) < 3 * math.pi / 2:
+        corrected_angle = theta - np.sign(theta) * math.pi
+        qc = QuantumCircuit(*qr)
+        qc.rx(math.pi, q0)
+        qc.rx(math.pi, q1)
+        qc.rxx(corrected_angle, q0, q1)
+        return qc
+
+    corrected_angle = theta - np.sign(theta) * 2 * math.pi
+    qc = QuantumCircuit(*qr)
+    qc.rxx(corrected_angle, q0, q1)
+    return qc
+
+
+class WrapRxxAngles(TransformationPass):
+    """Wrap Rxx angles to [-π/2, π/2]."""
+
+    def run(self, dag: DAGCircuit) -> DAGCircuit:
+        for node in dag.gate_nodes():
+            if node.name == "rxx":
+                (theta,) = node.op.params[0]
+
+                if abs(float(theta)) <= math.pi / 2:
+                    continue
+
+                q0, q1 = node.qargs
+                qc = wrap_rxx_angle(float(theta), q0, q1)
+                dag.substitute_node_with_dag(node, circuit_to_dag(qc))
+
+        return dag
+
+
+class AQTTranslationPlugin(PassManagerStagePlugin):
+    def pass_manager(
+        self, pass_manager_config: PassManagerConfig, optimization_level: Optional[int] = None
+    ) -> PassManager:
+        translation_pm = common.generate_translation_passmanager(
+            target=pass_manager_config.target,
+            basis_gates=pass_manager_config.basis_gates,
+            approximation_degree=pass_manager_config.approximation_degree,
+            coupling_map=pass_manager_config.coupling_map,
+            backend_props=pass_manager_config.backend_properties,
+            unitary_synthesis_method=pass_manager_config.unitary_synthesis_method,
+            unitary_synthesis_plugin_config=pass_manager_config.unitary_synthesis_plugin_config,
+            hls_config=pass_manager_config.hls_config,
+        )
+
+        passes: List[BasePass] = [WrapRxxAngles()]
+
+        return translation_pm + PassManager(passes)
--- test/test_transpilation.py
+++ test/test_transpilation.py
@@ -11,12 +11,18 @@
 # that they have been altered from the originals.
 
 from math import pi
+from typing import Final
 
 import pytest
-from qiskit import QuantumCircuit, transpile
+from qiskit import QuantumCircuit, QuantumRegister, transpile
 
 from qiskit_aqt_provider.aqt_resource import AQTResource
-from qiskit_aqt_provider.test.circuits import assert_circuits_equal
+from qiskit_aqt_provider.test.circuits import (
+    assert_circuits_equal,
+    assert_circuits_equivalent,
+    qft_circuit,
+)
+from qiskit_aqt_provider.transpiler_plugin import wrap_rxx_angle
 
 
 @pytest.mark.parametrize(
@@ -34,13 +40,13 @@ def test_rx_wrap_angle(
     """Check that transpiled rotation gate angles are wrapped to [-π,π]."""
     qc = QuantumCircuit(1)
     qc.rx(angle, 0)
-    qc.measure_all()
 
     expected = QuantumCircuit(1)
     expected.r(expected_angle, 0, 0)
-    expected.measure_all()
 
     result = transpile(qc, offline_simulator_no_noise, optimization_level=3)
+    assert isinstance(result, QuantumCircuit)
+
     assert_circuits_equal(result, expected)
 
 
@@ -48,13 +54,13 @@ def test_rx_r_rewrite_simple(offline_simulator_no_noise: AQTResource) -> None:
     """Check that Rx gates are rewritten as R gates."""
     qc = QuantumCircuit(1)
     qc.rx(pi / 2, 0)
-    qc.measure_all()
 
     expected = QuantumCircuit(1)
     expected.r(pi / 2, 0, 0)
-    expected.measure_all()
 
     result = transpile(qc, offline_simulator_no_noise, optimization_level=3)
+    assert isinstance(result, QuantumCircuit)  # only got one circuit back
+
     assert_circuits_equal(result, expected)
 
 
@@ -63,12 +69,91 @@ def test_decompose_1q_rotations_simple(offline_simulator_no_noise: AQTResource) 
     qc = QuantumCircuit(1)
     qc.rx(pi / 2, 0)
     qc.ry(pi / 2, 0)
-    qc.measure_all()
 
     expected = QuantumCircuit(1)
     expected.rz(-pi / 2, 0)
     expected.r(pi / 2, 0, 0)
-    expected.measure_all()
 
     result = transpile(qc, offline_simulator_no_noise, optimization_level=3)
+    assert isinstance(result, QuantumCircuit)  # only got one circuit back
+
     assert_circuits_equal(result, expected)
+
+
+RXX_ANGLES: Final = [
+    pi / 4,
+    pi / 2,
+    -pi / 2,
+    3 * pi / 4,
+    -3 * pi / 4,
+    15 * pi / 8,
+    -15 * pi / 8,
+    33 * pi / 16,
+    -33 * pi / 16,
+]
+
+
+@pytest.mark.parametrize("angle", RXX_ANGLES)
+def test_rxx_wrap_angle(angle: float) -> None:
+    """Check that the circuit returned by `wrap_rxx_angle`
+    is equivalent to an Rxx operation with the passed angle."""
+
+    qr = QuantumRegister(2)
+    q0, q1 = qr._bits
+    qc = wrap_rxx_angle(angle, q0, q1)
+
+    # one rxx in, one rxx out!
+    assert set(qc.count_ops()) <= {"rxx", "rz", "rx", "ry", "r"}
+    assert qc.count_ops()["rxx"] == 1
+
+    expected = QuantumCircuit(2)
+    expected.rxx(angle, 0, 1)
+
+    assert_circuits_equivalent(qc, expected)
+
+
+@pytest.mark.parametrize("angle", RXX_ANGLES)
+def test_rxx_wrap_angle_transpile(angle: float, offline_simulator_no_noise: AQTResource) -> None:
+    """Check that Rxx angles are wrapped by the transpiler."""
+    qc = QuantumCircuit(2)
+    qc.rxx(angle, 0, 1)
+    trans_qc = transpile(qc, offline_simulator_no_noise, optimization_level=3)
+    assert isinstance(trans_qc, QuantumCircuit)
+
+    assert set(trans_qc.count_ops()) <= set(offline_simulator_no_noise.configuration().basis_gates)
+    assert trans_qc.count_ops()["rxx"] == 1
+
+    # check that all Rxx have angles in [-π/2, π/2]
+    for operation in trans_qc.data:
+        if operation[0] == "rxx":
+            (theta,) = operation.params
+            assert abs(float(theta)) <= pi / 2
+
+    # check that the transpiled circuit is equivalent to the original one
+    assert_circuits_equivalent(trans_qc, qc)
+
+
+@pytest.mark.parametrize("qubits", [1, 5, 10])
+@pytest.mark.parametrize("optimization_level", [0, 1, 2, 3])
+def test_qft_circuit_transpilation(
+    qubits: int, optimization_level: int, offline_simulator_no_noise: AQTResource
+) -> None:
+    """Transpile a N-qubit QFT circuit for an AQT backend. Check that the angles are properly
+    wrapped."""
+    qc = qft_circuit(qubits)
+    trans_qc = transpile(qc, offline_simulator_no_noise, optimization_level=optimization_level)
+    assert isinstance(trans_qc, QuantumCircuit)
+
+    assert set(trans_qc.count_ops()) <= set(offline_simulator_no_noise.configuration().basis_gates)
+
+    for operation in trans_qc.data:
+        if operation[0] == "rxx":
+            (theta,) = operation.params
+            assert abs(float(theta)) <= pi / 2
+
+        if operation[0] == "r":
+            (theta, _) = operation.params
+            assert abs(theta) <= pi
+
+    if optimization_level < 3 and qubits < 6:
+        assert_circuits_equivalent(qc, trans_qc)
